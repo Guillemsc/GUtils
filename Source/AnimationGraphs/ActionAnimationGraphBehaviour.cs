2ec92fd9153a6ad929de559cac811b1b47316742
Guillem Sunyer <gsunyercaldu@gmail.com>
2023-12-17 22:23:46 +0100

    Head

---
+++ Source/AnimationGraphs/ActionAnimationGraphBehaviour.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace GUtils.AnimationGraphs;
+
+public sealed class ActionAnimationGraphBehaviour : AnimationGraphBehaviour
+{
+    readonly Action _action;
+
+    public ActionAnimationGraphBehaviour(Action action)
+    {
+        _action = action;
+    }
+
+    public override void Enter()
+    {
+        _action.Invoke();
+        Completed = true;
+    }
+
+    public override void Tick()
+    {
+
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
